e48f54a7d2092a8232ad31662eae5a134c015b77
Alex-hcl <74538462+Alex-hcl@users.noreply.github.com>
2024-07-24 15:25:17 -0700

    Update route.ts，删除了catch error报错

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -4,10 +4,8 @@ import { NextResponse } from 'next/server';
 import connection from '../../../utils/db'; // 确保路径正确
 
 export async function GET() {
-  try {
+  
     const [rows] = await connection.query('SELECT * FROM users');
     return NextResponse.json(rows);
-  } catch (error) {
-    return NextResponse.json({ error: error.message }, { status: 500 });
-  }
+
 }
